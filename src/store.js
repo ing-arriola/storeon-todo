e0e874cc549fe0d91ef5df04eecc97874e8f1cfb
Jaime <ingeniero.arriola@gmail.com>
2021-04-26 10:52:15 -0600

    created store

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import {createStoreon} from 'storeon'
+import {v4 as uuidv4} from 'uuid'
+import {} from '@storeon/localstorage'
+
+let note = store => {
+    store.on('@init',() => ({
+        tasks:[
+            {id:uuidv4(),item:"first task"},
+            {id:uuidv4(),item:"second task"}
+        ]
+    }))
+    store.on('addTask',({tasks},task)=>{
+        return {
+            tasks: [...tasks,{id:uuidv4().item:task}] 
+        }
+    })
+    store.on('deleteTask',({tasks,id})=>({
+        tasks: tasks.filter(note => note.id === id)
+    }))
+}
